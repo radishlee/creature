cbf23fbbaa738624ce28ed477edddf4e314485a0
李睿博(liruibo) <liruibo@51talk.com>
2017-11-14 19:07:08 +0800

    add new project

--- service/src/test/java/com/lunchtech/vlbs/service/FileServiceITTest.java
+++ service/src/test/java/com/lunchtech/vlbs/service/FileServiceITTest.java
@@ -1,15 +1,15 @@
-package com.lunchtech.vlbs.service;
-
-import com.alibaba.fastjson.JSONObject;
-import com.lunchtech.vlbs.common.enumuration.FileTypeEnum;
-import com.lunchtech.vlbs.common.exception.ServiceException;
-import org.junit.Before;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.Assert;
-
-public class FileServiceITTest extends BaseITTest {
-    @Autowired
-    private UserService fileService;
-
-}
+//package com.lunchtech.vlbs.service;
+//
+//import com.alibaba.fastjson.JSONObject;
+//import com.lunchtech.vlbs.common.enumuration.FileTypeEnum;
+//import com.lunchtech.vlbs.common.exception.ServiceException;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.util.Assert;
+//
+//public class FileServiceITTest extends BaseITTest {
+//    @Autowired
+//    private UserService fileService;
+//
+//}
---
+++ uploader/src/main/java/com/lunchtech/vlbs/api/controller/FileController.java
@@ -0,0 +1,25 @@
+package com.lunchtech.vlbs.api.controller;
+
+import com.lunchtech.vlbs.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/file")
+public class FileController {
+    @Autowired
+    private UserService fileService;
+
+    @RequestMapping(value = "/preFile", method = RequestMethod.GET)
+    public Object getPrevicousFile(String name, Integer type) {
+        return "aaa";
+    }
+
+
+    @RequestMapping(value = "/latFile", method = RequestMethod.GET)
+    public Object getLatestFile(String name, Integer type) {
+        return "aaa";
+    }
+}
